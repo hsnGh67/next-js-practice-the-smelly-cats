9bf5db8b857ae0d866658b14d05777d538a9d075
hasan <hasan.ghanavi@gmail.com>
2022-01-24 11:01:34 +0330

    create show model

---
+++ database/models/show.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const show = mongoose.Schema({
+    slug:{
+        unique:[true,'The slug must be unique'],
+        required:[true,'The slug is required'],
+        type:String,
+        maxlength:250
+    },
+    title:{
+        required:[true,'The title is required'],
+        type:String,
+        maxlength:250
+    },
+    venue:{
+        required:[true,'The venue is required'],
+        type:String,
+        maxlength:250
+    },
+    excerpt:{
+        type:String,
+        required:[true,'The excerpt is required'],
+        maxlength:2000
+    },
+    content:{
+        type:String,
+        required:[true,'The content is required'],
+        maxlength:10000
+    },
+    yt:{
+        type:String,
+        required:[true,'The yt link is required'],
+        maxlength:250
+    },
+    image:{
+        type:String,
+        maxlength:2000,
+        default:'na.jpg'
+    },
+    date:{
+        type:String,
+        required:[true,'The date is required']
+    },
+    time:{
+        type:String,
+        required:[true,'The time is required']
+    }
+});
+
+export default mongoose.models.Show || new mongoose.model("Show" , show)
